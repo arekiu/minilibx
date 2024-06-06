0a44c77e8e47859999dd8b01937a158561e046d9
Alexis Mariel Schmidt <aschmidt@c4a7c8.42berlin.de>
2024-06-06 15:56:12 +0200

    first commit

---
+++ example.c
@@ -0,0 +1,12 @@
+#include <mlx.h>
+
+int	main()
+{
+	void	*mlx_connection;
+	void	*mlx_window;
+
+	mlx_connection = mlx_init();
+	mlx_window = mlx_new_window(mlx_connection, 500, 500, "My first window");
+	mlx_loop(mlx_connection);
+}
+
